1c01c922565e090dbf3a8ea0dfecb0418ae15e21
Martin Buchmann <Martin.Buchmann@googlemail.com>
2018-12-28 10:07:30 +0100

    Implemented the data parsing for day 7.

--- 7.lisp
+++ 7.lisp
@@ -1,9 +1,13 @@
 (in-package :aoc-18)
 (annot:enable-annot-syntax)
 
-;;; TODO: Read and parse input into a tree
-<<<<<<< HEAD
+(defun read-input7 (&optional (input #p"inputs/input7.txt"))
+  "Reads the input for AOC-18 day 7."
+  (iter
+    (for line in-file input using #'read-line)
+    (collect (register-groups-bind (a b)
+                 ("Step (\\w) must be finished before step (\\w) can begin." line)
+               (list a b)))))
+
 ;;; TODO: Do a topological sort on the tree
-=======
-;;; Do a topological sort on the tree
->>>>>>> 8bb36e75322bd78d2737bbac6bd9b2f5d0eb473b
+
