e7d64af73d4b4c17df3eb60cba3662286de5fa3c
Martin Buchmann <Martin.Buchmann@googlemail.com>
2018-12-28 13:48:40 +0100

    My attempt to solve part 1 of day 7
    Although the example data is working my solution fails for for my input
    data. Maybe the topological sort from cl-digraph retrieves another order than
    expected. I do not have the time to implement my own sorting so I will just stop
    here :smiley:

--- 7.lisp
+++ 7.lisp
@@ -9,5 +9,35 @@
                  ("Step (\\w) must be finished before step (\\w) can begin." line)
                (list a b)))))
 
-;;; TODO: Do a topological sort on the tree
+(defun instruction-graph (instructions &aux (graph (digraph:make-digraph :test #'equal)))
+  (iter
+    (for (a b) in instructions)
+    (digraph:insert-vertex graph a)
+    (digraph:insert-vertex graph b)
+    (digraph:insert-edge graph a b)
+    (dbg :07 ";;; ~A ~A~%" a b))
+  (digraph.dot:draw graph :filename "graph.png" :format :png)
+  graph)
+
+(defun sort-graph (graph &aux (result (make-array 0
+                                        :element-type 'character
+                                        :fill-pointer 0
+                                        :adjustable t)))
+  (iter
+    (for s in (digraph:topological-sort graph))
+    (vector-push-extend (coerce s 'character) result))
+  result)
+
+(digraph:count-vertices *instructions*)
+(digraph:count-edges *instructions*)
+
+(defun aoc18-07a (&optional (input #p"inputs/input7.txt"))
+  "The first part of day 7."
+  (-> input
+      (read-input7)
+      (instruction-graph)
+      (sort-graph)
+      (nreverse)))
+
+;;; TSLGXBINDKVPJMUFCAHEOWYQRZ
 
